9c49e977fcfc8eb70bdc4dd1d71c9f8bcb135fb5
Shyam-SN <snshyam747@gmail.com>
2025-03-28 08:07:42 +0530

    Fix CMakeSystem.cmake and related components

---
+++ build/CMakeFiles/FedEdgeAsync.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FedEdgeAsync.dir/src/GrpcClient.cpp.o"
+  "CMakeFiles/FedEdgeAsync.dir/src/GrpcClient.cpp.o.d"
+  "CMakeFiles/FedEdgeAsync.dir/src/SecureAggregator.cpp.o"
+  "CMakeFiles/FedEdgeAsync.dir/src/SecureAggregator.cpp.o.d"
+  "CMakeFiles/FedEdgeAsync.dir/src/main.mm.o"
+  "CMakeFiles/FedEdgeAsync.dir/src/main.mm.o.d"
+  "FedEdgeAsync"
+  "FedEdgeAsync.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX OBJCXX)
+  include(CMakeFiles/FedEdgeAsync.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
