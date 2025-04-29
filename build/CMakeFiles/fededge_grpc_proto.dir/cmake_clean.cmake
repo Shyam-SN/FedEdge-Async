5be9bda4ab27ad21d5e62fb1d7eaed95fb63c339
Shyam-SN <snshyam747@gmail.com>
2025-04-30 00:48:23 +0530

    Fix symbol_folder_index.txt and related components

---
+++ build/CMakeFiles/fededge_grpc_proto.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fededge_grpc_proto.dir/generated/fededge.grpc.pb.cc.o"
+  "CMakeFiles/fededge_grpc_proto.dir/generated/fededge.grpc.pb.cc.o.d"
+  "CMakeFiles/fededge_grpc_proto.dir/generated/fededge.pb.cc.o"
+  "CMakeFiles/fededge_grpc_proto.dir/generated/fededge.pb.cc.o.d"
+  "generated/fededge.grpc.pb.cc"
+  "generated/fededge.grpc.pb.h"
+  "generated/fededge.pb.cc"
+  "generated/fededge.pb.h"
+  "libfededge_grpc_proto.a"
+  "libfededge_grpc_proto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fededge_grpc_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
